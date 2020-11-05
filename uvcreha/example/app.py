3210b86d54aa7fd213765387bb180d623a2f3f48
Souheil Chelfouh <trollfot@gmail.com>
2020-11-05 21:07:12 +0100

    Fixing style

--- uvcreha/example/app.py
+++ uvcreha/example/app.py
@@ -1,21 +1,17 @@
 from docmanager.app import application
 from docmanager.request import Request
-from horseman.response import reply
-from functools import cached_property
+from horseman.response import Response
 
 
 class CustomRequest(Request):
-
-    @cached_property
-    def database_session(self):
-        return self.app.database.new_session()
+    pass
 
 
 @application.routes.register('/myview')
 def my_view(request: CustomRequest):
-    return reply(body=u"HALLO WELT")
+    return Response.create(body="HALLO WELT")
 
 
 @application.ui.register_slot(request=CustomRequest, name="sitecap")
 def render_slot(request, name):
-    return reply(body=u"HALLO WELT")
+    return Response.create(body="HALLO WELT")
