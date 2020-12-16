eb9edb6412a0074604903c41c887d9e3c78ce30a
Christian Klinger <ck@novareto.de>
2020-12-16 18:05:51 +0100

    changes

--- uvcreha/example/views.py
+++ uvcreha/example/views.py
@@ -4,44 +4,23 @@ from docmanager.app import browser as application
 from docmanager.models import Document
 from docmanager.browser.form import DocFormView, Form
 from docmanager.request import Request
-from reiter.routing.predicate_route import BranchingView, PredicateError
 
 from horseman.http import Multidict
 from .models import SomeDocument
 from reiter.form import trigger
 
 
-@application.routes.register("/users/{username}/files/{az}/docs/{key}", methods=["GET"], name="event_edit")
-class DocumentIndex(BranchingView):
-    pass
+@application.routes.register(
+    "/users/{username}/files/{az}/docs/{key}", methods=["GET"], name="event_edit"
+)
+def document_index(request: Request, **kwargs):
+    import pdb; pdb.set_trace()
+    return EventEditForm(request)()
 
 
-def not_state_inquiry(request):
-    document = request.database(Document).fetch(
-        request.route.params.get("key")
-    )
-    if document.state == "inquiry":
-        raise PredicateError.create(400, 'Condition must be test')
-    return
-
-
-def state_inquiry(request):
-    document = request.database(Document).fetch(
-        request.route.params.get("key")
-    )
-    if document.state != "inquiry":
-        raise PredicateError.create(400, 'Condition must be test')
-    return
-
-
-@DocumentIndex.register(["GET"], not_state_inquiry)
-def test_branching(request: Request, *args, **kwargs):  #condition, username, az, key):
-    return horseman.response.reply(200, "Yeah, i'm a test")
-
-
-# @application.routes.register(
-#    "/users/{username}/files/{az}/docs/{key}/edit", name="event_edit")
-@DocumentIndex.register(["GET", "POST"], state_inquiry)
+@application.routes.register(
+    "/users/{username}/files/{az}/docs/{key}/edit", name="vent_edit"
+)
 class EventEditForm(DocFormView):
 
     title = "Form"
@@ -56,7 +35,7 @@ class EventEditForm(DocFormView):
 
     @trigger("speichern", "Speichern", css="btn btn-primary")
     def speichern(self, request, data):
-        document = request.database(BaseDocument)
+        document = request.database(Document)
         document.fetch(request.route.params.get("key"))
         form = self.setupForm(formdata=data.form)
         if not form.validate():
